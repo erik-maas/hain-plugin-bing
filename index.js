90fc817a4a06d2d99e40c6cba65e78048b6d6707
Erik Maas <contact@companyou.eu>
2016-03-31 09:54:04 +0200

    index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict'
+
+module.exports = (pluginContext) => {
+  const shell = pluginContext.shell
+
+  function search (query, res) {
+    const query_trim = query.trim()
+
+    if (query_trim.length === 0) {
+      return
+    }
+
+    res.add({
+      id: query_trim,
+      payload: 'open',
+      title: query_trim,
+      desc: 'Search on Bing.com'
+    })
+  }
+
+  function execute (id, payload) {
+    if (payload !== 'open') {
+      return
+    }
+    shell.openExternal(`http://www.bing.com/search?q=${id}`)
+  }
+
+  return { search, execute }
+}
